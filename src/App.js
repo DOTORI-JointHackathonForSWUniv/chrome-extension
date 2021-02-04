6ec9d3302a0bfb5bd3e02139e4e832ef3b4f4c0a
broccolism <mile3880@gmail.com>
2021-02-05 01:39:06 +0900

    feat: resize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // chrome.windows.getLastFocused({ populate: false }, function (currentWindow) {
+  //   chrome.windows.update(currentWindow.id, { width: 500 });
+  // });
+  // chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, { width: 500 });
   return (
     <div className="App">
       <header className="App-header">
