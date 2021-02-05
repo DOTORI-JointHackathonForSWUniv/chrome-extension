7722d0b9d15ba010bcf91e8647c67c7e0d0aaa6c
Ji Eun Han <hanji0514@ajou.ac.kr>
2021-02-05 22:30:49 +0900

    b4 merge

--- src/pages/GitAdd.js
+++ src/pages/GitAdd.js
@@ -4,6 +4,7 @@ import basket from "../assets/basket.png";
 import step from "../assets/step.png";
 import styled, { keyframes, css } from "styled-components";
 import Header from "./Header";
+import * as db from "../apis/firebase";
 
 const Wrapper = styled.div`
     background-color: #ffffff;
@@ -13,32 +14,32 @@ const Wrapper = styled.div`
     height: 99%;
 `;
 const StepBox = styled.span`
-  display: flex;
-  flex-direction: row;
-  padding-top: 22px;
+    display: flex;
+    flex-direction: row;
+    padding-top: 22px;
 `;
 const Step = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0 30px;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin: 0 30px;
+    align-items: center;
 `;
 const StepButton = styled.img`
-  width: 10px;
-  height: 9px;
+    width: 10px;
+    height: 9px;
 `;
 
 const StepText = styled.div`
-  padding-top: 11px;
-  font-size: 15px;
-  color: #d2d2d2;
+    padding-top: 11px;
+    font-size: 15px;
+    color: #d2d2d2;
 `;
 
 const ImgBox = styled.div`
-  padding: 5rem 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+    padding: 5rem 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 `;
 const moveAnimation = keyframes`
  0% {
@@ -83,10 +84,51 @@ const AddButton = styled.button`
 `;
 
 const GitAdd = ({ setPage }) => {
+    const [entryCode, setEntryCode] = useState({});
     const [clicked, setClicked] = useState(false);
     const toggleClicked = () => setClicked((value) => !value);
     // const [clicked2, setClicked2] = useState(false);
     // const toggleClicked2 = () => setClicked2(value => !value);
+
+    const exportSourceEvent = () => {
+        console.log("export click");
+
+        chrome.tabs.getSelected((current_tab) => {
+            const current_tab_id = current_tab.id;
+            chrome.storage.local.set({ export_response_display: "" }, () => {});
+            chrome.tabs.sendMessage(current_tab_id, {
+                type: "export_request",
+                source: "popup.js",
+                destination: "contentScript.js",
+            });
+        });
+    };
+
+    useEffect(() => {
+        chrome.storage.onChanged.addListener((changes, namespace) => {
+            for (var key in changes) {
+                var storageChange = changes[key];
+                // console.log(
+                //     'Storage key "%s" in namespace "%s" changed. ' +
+                //         'Old value was "%s", new value is "%s".',
+                //     key,
+                //     namespace,
+                //     storageChange.oldValue,
+                //     storageChange.newValue
+                // );
+
+                if (key === "export_response_display") {
+                    // console.log("Success");
+                    // console.log(JSON.stringify(storageChange.newValue));
+                    setEntryCode(storageChange.newValue);
+                }
+            }
+        });
+    }, []);
+
+    const gitAdd = async () => {
+        await db.gitAdd(entryCode);
+    };
 
     return (
         <Wrapper>
@@ -94,25 +136,37 @@ const GitAdd = ({ setPage }) => {
             <StepBox>
                 <Step>
                     <StepButton src={step}></StepButton>
-                    <StepText style={{ color: "#2ed37e" }}>도토리 넣기</StepText>
+                    <StepText style={{ color: "#2ed37e" }}>
+                        도토리 넣기
+                    </StepText>
                 </Step>
                 <Step>
-                    <StepText style={{ paddingTop: "20px" }}>주머니 이름 정하기</StepText>
+                    <StepText style={{ paddingTop: "20px" }}>
+                        주머니 이름 정하기
+                    </StepText>
                 </Step>
                 <Step>
-                    <StepText style={{ paddingTop: "20px" }}>도토리 보관하기</StepText>
+                    <StepText style={{ paddingTop: "20px" }}>
+                        도토리 보관하기
+                    </StepText>
                 </Step>
             </StepBox>
             <ImgBox>
-                <DotoriImg src={dotori} clicked={clicked} onClick={toggleClicked}></DotoriImg>
+                <DotoriImg
+                    src={dotori}
+                    clicked={clicked}
+                    onClick={toggleClicked}
+                ></DotoriImg>
                 <BasketImg src={basket}></BasketImg>
             </ImgBox>
             <AddButton
                 onClick={() => {
                     toggleClicked();
                     setTimeout(() => setPage("commit"), 3000); //5초 딜레이
+                    exportSourceEvent();
+                    gitAdd();
                 }}
-                // onClick={toggleClicked}
+
             >
                 주머니에 내가 만든 도토리 넣기
             </AddButton>
