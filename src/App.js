c881ef7af94fde196f1fa91e5c114667621a28bf
minjyo <minjyo8823@gmail.com>
2021-02-05 13:06:55 +0900

    Add GitCommit page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 // import FirebaseTest from "./pages/FirebaseTest";
 import GitAdd from "./pages/GitAdd";
+import GitCommit from "./pages/GitCommit";
 
 function App() {
-  // chrome.windows.getLastFocused({ populate: false }, function (currentWindow) {
-  //   chrome.windows.update(currentWindow.id, { width: 500 });
-  // });
-  // chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, { width: 500 });
+    // chrome.windows.getLastFocused({ populate: false }, function (currentWindow) {
+    //   chrome.windows.update(currentWindow.id, { width: 500 });
+    // });
+    // chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, { width: 500 });
 
-  return (
-    <div className="App">
-      <GitAdd />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/add" component={GitAdd} exact></Route>
+                <Route path="/commit" component={GitCommit} exact></Route>
+                <Route render={({ location }) => <div>이 페이지는 존재하지 않습니다:{location.pathname}</div>}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/GitCommit.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import step from "../assets/step.png";
+import commit from "../assets/commit.png";
+import styled from "styled-components";
+import Header from "./Header";
+
+const Wrapper = styled.div`
+    background-color: #ffffff;
+    border-style: solid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 99%;
+`;
+const StepBox = styled.span`
+    display: flex;
+    flex-direction: row;
+    padding-top: 22px;
+`;
+const Step = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 30px;
+    align-items: center;
+`;
+const StepButton = styled.img`
+    width: 10px;
+    height: 9px;
+`;
+
+const StepText = styled.div`
+    padding-top: 11px;
+    font-size: 15px;
+    color: #d2d2d2;
+`;
+
+const CommitBox = styled.div`
+    padding: 4rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const CommitText = styled.div`
+    font-size: 14px;
+    color: #332820;
+    position: relative;
+`;
+const CommitImg = styled.img`
+    width: 215px;
+    height: 215px;
+`;
+const CommitInput = styled.input`
+    width: 255px;
+    height: 44px;
+    border: solid 1px #707070;
+    position: absolute;
+    margin-top: 200px;
+    /* padding-top: -1rem; */
+`;
+
+const AddButton = styled.button`
+    border: none;
+    width: 460px;
+    height: 70px;
+    border-radius: 5px;
+    text-align: center;
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+const GitCommit = ({ history }) => {
+    const [inputName, setInputName] = useState("");
+    const [isTyping, setTyping] = useState(false);
+
+    const movePage = (page) => {
+        history.push(`/${page}`);
+    };
+
+    return (
+        <Wrapper>
+            <Header></Header>
+            <StepBox>
+                <Step>
+                    <StepText style={{ paddingTop: "20px" }}>도토리 넣기</StepText>
+                </Step>
+                <Step>
+                    <StepButton src={step}></StepButton>
+                    <StepText style={{ color: "#2ed37e" }}>주머니 이름 정하기</StepText>
+                </Step>
+                <Step>
+                    <StepText style={{ paddingTop: "20px" }}>도토리 보관하기</StepText>
+                </Step>
+            </StepBox>
+            <CommitBox>
+                <CommitText>주머니 이름 정해줘~ 먹을 때 찾기 편하게 :)</CommitText>
+                <CommitImg src={commit}></CommitImg>
+                <CommitInput
+                    value={inputName}
+                    onChange={(e) => {
+                        setInputName(e.target.value);
+                        setTyping(true);
+                    }}
+                ></CommitInput>
+            </CommitBox>
+            <AddButton
+                onClick={() => {
+                    movePage("GitPush");
+                }}
+                style={{ backgroundColor: `${isTyping ? " #2ed37e" : "#e5e5e5"}` }}
+            >
+                주머니 이름 정하기
+            </AddButton>
+        </Wrapper>
+    );
+};
+
+export default GitCommit;
