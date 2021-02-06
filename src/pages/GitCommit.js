0c657a5e6259d752bcf2f3a5ad1fc1c03b4fd7c3
broccolism <mile3880@gmail.com>
2021-02-06 10:21:19 +0900

    fixing

--- src/pages/GitCommit.js
+++ src/pages/GitCommit.js
@@ -6,16 +6,16 @@ import Header from "../components/Header";
 import * as db from "../apis/firebase";
 
 const CommitBox = styled.div`
-    padding: ${(props) => {
-        if (props.complete === "doing") {
-            return "3rem 0 3rem 0";
-        } else {
-            return "5rem 0";
-        }
-    }};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  padding: ${(props) => {
+    if (props.complete === "doing") {
+      return "3rem 0 3rem 0";
+    } else {
+      return "5rem 0";
+    }
+  }};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const BigAnimation = keyframes`
@@ -35,122 +35,113 @@ const SmallAnimation = keyframes`
  } 
 `;
 const CommitImg = styled.img`
-    width: 215px;
-    animation-name: ${({ complete }) =>
-        complete === "doing"
-            ? css`
-                  ${BigAnimation};
-              `
-            : complete === "done"
-            ? css`
-                  ${SmallAnimation};
-              `
-            : null};
-    animation-duration: 2s;
-    animation-iteraion-count: infinite;
-    animation-fill-mode: forwards;
+  width: 215px;
+  animation-name: ${({ complete }) =>
+    complete === "doing"
+      ? css`
+          ${BigAnimation};
+        `
+      : complete === "done"
+      ? css`
+          ${SmallAnimation};
+        `
+      : null};
+  animation-duration: 2s;
+  animation-iteraion-count: infinite;
+  animation-fill-mode: forwards;
 `;
 const CommitText = styled.div`
-    font-size: 14px;
-    color: #332820;
-    margin-top: -20px;
+  font-size: 14px;
+  color: #332820;
+  margin-top: -20px;
 `;
 const CommitInput = styled.input`
-    width: 255px;
-    height: 44px;
-    font-weight: bold;
-    border: solid 1px #707070;
-    margin-top: 16px;
-    text-align: center;
+  width: 255px;
+  height: 44px;
+  font-weight: bold;
+  border: solid 1px #707070;
+  margin-top: 16px;
+  text-align: center;
 `;
 const CommitName = styled.div`
-    font-size: 16px;
-    font-weight: 800;
-    color: #332820;
-    height: 19px;
-    margin-bottom: -20px;
+  font-size: 16px;
+  font-weight: 800;
+  color: #332820;
+  height: 19px;
+  margin-bottom: -20px;
 `;
 const AddButton = styled.button`
-    border: none;
-    width: 460px;
-    height: 70px;
-    border-radius: 5px;
-    text-align: center;
-    color: #ffffff;
-    font-size: 20px;
-    font-weight: bold;
-    cursor: pointer;
+  border: none;
+  width: 460px;
+  height: 70px;
+  border-radius: 5px;
+  text-align: center;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
 `;
 
 //이전 커밋 내용 로그에 보이기!
 const GitCommit = ({ setPage, setName }) => {
-    const [inputName, setInputName] = useState("");
-    const [isTyping, setTyping] = useState(false);
-    const [complete, setComplete] = useState("start");
-    const toggleCompleted = (status) => {
-        console.log("clicked");
-        setComplete(status);
-    };
-    const [curData, setData] = useState([]);
+  const [inputName, setInputName] = useState("");
+  const [isTyping, setTyping] = useState(false);
+  const [complete, setComplete] = useState("start");
+  const toggleCompleted = (status) => {
+    console.log("clicked");
+    setComplete(status);
+  };
 
-    const getData = async () => {
-        const newData = await db.getTestData();
-        setData(curData.concat(newData));
-    };
+  const gitCommit = async () => {
+    setName(inputName);
+    await db.gitCommit(inputName);
+  };
 
-    // 최초 렌더링 이후에 실행하기 위해 useEffect 내부에서 함수 실행
-    useEffect(() => {
-        getData();
-    }, []);
-
-    const gitCommit = async () => {
-        setName(inputName);
-        await db.gitCommit(inputName);
-    };
-
-    return (
-        <div>
-            <CommitBox complete={complete}>
-                <CommitImg src={commit} complete={complete}></CommitImg>
-                {complete === "doing" ? <CommitText>주머니 이름 정해줘~ 먹을 때 찾기 편하게 :)</CommitText> : null}
-                {complete === "done" ? (
-                    <CommitName>{inputName}</CommitName>
-                ) : complete === "doing" ? (
-                    <CommitInput
-                        value={inputName}
-                        onChange={(e) => {
-                            setInputName(e.target.value);
-                            setTyping(true);
-                        }}
-                    ></CommitInput>
-                ) : null}
-            </CommitBox>
-            {complete === "start" ? (
-                <AddButton
-                    onClick={() => toggleCompleted("doing")}
-                    style={{
-                        backgroundColor: "#755e4c",
-                    }}
-                >
-                    도토리 넣은 주머니 이름 정하기
-                </AddButton>
-            ) : (
-                <AddButton
-                    onClick={() => {
-                        setTyping(false);
-                        gitCommit();
-                        toggleCompleted("done");
-                        setTimeout(() => setPage("push"), 3000); //5초 딜레이
-                    }}
-                    style={{
-                        backgroundColor: `${isTyping ? " #2ed37e" : "#e5e5e5"}`,
-                    }}
-                >
-                    주머니 이름 결정하기
-                </AddButton>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <CommitBox complete={complete}>
+        <CommitImg src={commit} complete={complete}></CommitImg>
+        {complete === "doing" ? (
+          <CommitText>주머니 이름 정해줘~ 먹을 때 찾기 편하게 :)</CommitText>
+        ) : null}
+        {complete === "done" ? (
+          <CommitName>{inputName}</CommitName>
+        ) : complete === "doing" ? (
+          <CommitInput
+            value={inputName}
+            onChange={(e) => {
+              setInputName(e.target.value);
+              setTyping(true);
+            }}
+          ></CommitInput>
+        ) : null}
+      </CommitBox>
+      {complete === "start" ? (
+        <AddButton
+          onClick={() => toggleCompleted("doing")}
+          style={{
+            backgroundColor: "#755e4c",
+          }}
+        >
+          도토리 넣은 주머니 이름 정하기
+        </AddButton>
+      ) : (
+        <AddButton
+          onClick={() => {
+            setTyping(false);
+            gitCommit();
+            toggleCompleted("done");
+            setTimeout(() => setPage("push"), 3000); //5초 딜레이
+          }}
+          style={{
+            backgroundColor: `${isTyping ? " #2ed37e" : "#e5e5e5"}`,
+          }}
+        >
+          주머니 이름 결정하기
+        </AddButton>
+      )}
+    </div>
+  );
 };
 
 export default GitCommit;
--- src/pages/GitPush.js
+++ src/pages/GitPush.js
@@ -5,17 +5,17 @@ import styled, { keyframes, css } from "styled-components";
 import * as db from "../apis/firebase";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    position: relative;
+  display: flex;
+  flex-direction: row;
+  position: relative;
 `;
 const ContentBox = styled.div``;
 const PrevCommitsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    z-index: 1;
-    position: absolute;
-    right: -110px;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  position: absolute;
+  right: -110px;
 `;
 const moveAnimation0 = keyframes`
  0% {
@@ -46,37 +46,37 @@ const moveAnimation1 = keyframes`
  } 
 `;
 const PrevCommitBox = styled.div`
-    animation-name: ${({ clicked, index }) =>
-        clicked && index === 0
-            ? css`
-                  ${moveAnimation0};
-              `
-            : clicked && index === 1
-            ? css`
-                  ${moveAnimation1};
-              `
-            : null};
-    animation-duration: 2s;
-    animation-iteraion-count: infinite;
-    animation-fill-mode: forwards;
+  animation-name: ${({ clicked, index }) =>
+    clicked && index === 0
+      ? css`
+          ${moveAnimation0};
+        `
+      : clicked && index === 1
+      ? css`
+          ${moveAnimation1};
+        `
+      : null};
+  animation-duration: 2s;
+  animation-iteraion-count: infinite;
+  animation-fill-mode: forwards;
 `;
 const PrevCommit = styled.img`
-    width: 68px;
-    height: 68px;
-    margin-top: 25px;
+  width: 68px;
+  height: 68px;
+  margin-top: 25px;
 `;
 const PrevCommitName = styled.div`
-    font-size: 10px;
-    color: #332820;
-    margin-top: 8px;
-    text-align: center;
+  font-size: 10px;
+  color: #332820;
+  margin-top: 8px;
+  text-align: center;
 `;
 
 const PushBox = styled.div`
-    padding: 5rem 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  padding: 5rem 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 const moveAnimation = keyframes`
  0% {
@@ -89,115 +89,108 @@ const moveAnimation = keyframes`
      transform: translateX(125%)
      animation-timing-function: cubic-bezier(0.33333, 0, 0.66667, 0.33333) }
  100% {
-   transform: scale(1) translateX(256%) translateY(50%);
+   transform: scale(1) translateX(290%) translateY(50%);
  } 
 `;
 const CommitBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    position: relative;
-    animation-name: ${({ clicked }) =>
-        clicked
-            ? css`
-                  ${moveAnimation};
-              `
-            : null};
-    animation-duration: 2s;
-    animation-iteraion-count: infinite;
-    animation-fill-mode: forwards;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  animation-name: ${({ clicked }) =>
+    clicked
+      ? css`
+          ${moveAnimation};
+        `
+      : null};
+  animation-duration: 2s;
+  animation-iteraion-count: infinite;
+  animation-fill-mode: forwards;
 `;
 const CommitName = styled.div`
-    font-size: 14px;
-    /* font-weight: 800; */
-    color: #332820;
-    margin-top: 1rem;
+  font-size: 14px;
+  /* font-weight: 800; */
+  color: #332820;
+  margin-top: 1rem;
 `;
 const CommitImg = styled.img`
-    width: 113px;
-    height: 113px;
+  width: 113px;
+  height: 113px;
 `;
 const HomeImg = styled.img`
-    width: 225px;
-    height: 225px;
-    margin-left: 10rem;
+  width: 225px;
+  height: 225px;
+  margin-left: 10rem;
 `;
 const AddButton = styled.button`
-    border: none;
-    width: 460px;
-    height: 70px;
-    border-radius: 5px;
-    text-align: center;
-    color: #ffffff;
-    font-size: 20px;
-    font-weight: bold;
-    cursor: pointer;
-    margin-top: -20px;
-    margin-left: 30px;
+  border: none;
+  width: 460px;
+  height: 70px;
+  border-radius: 5px;
+  text-align: center;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: -20px;
+  margin-left: 30px;
 `;
 
 const GitPush = ({ name }) => {
-    const [complete, setComplete] = useState(false);
+  const [complete, setComplete] = useState(false);
+  const [curData, setData] = useState([]);
 
-    const getData = async () => {
-        const newData = await db.getTestData();
-        setData(curData.concat(newData));
-    };
+  const gitPush = async () => {
+    await db.gitPush();
+  };
 
-    const [curLog, setCurLog] = useState([]);
-    const gitLogNotPushed = async () => {
-        const newLog = await db.gitLog();
-        setCurLog(newLog.slice(0, 2));
-        console.log(newLog);
-    };
+  const [curLog, setCurLog] = useState([]);
+  const gitLogNotPushed = async () => {
+    const newLog = await db.gitLog();
+    setCurLog(newLog.slice(0, 2));
+    console.log(newLog);
+  };
 
-    // 최초 렌더링 이후에 실행하기 위해 useEffect 내부에서 함수 실행
-    useEffect(() => {
-        gitLogNotPushed();
-        getData();
-    }, []);
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((value) => !value);
 
-    const gitPush = async () => {
-        await db.gitPush();
-    };
-
-    const [clicked, setClicked] = useState(false);
-    const toggleClicked = () => setClicked((value) => !value);
-
-    return (
-        <Wrapper>
-            <ContentBox>
-                <PushBox>
-                    <CommitBox clicked={clicked} onClick={toggleClicked}>
-                        <CommitImg src={commit}></CommitImg>
-                        <CommitName>{name}</CommitName>
-                    </CommitBox>
-                    <HomeImg src={home}></HomeImg>
-                </PushBox>
-                <AddButton
-                    onClick={() => {
-                        toggleClicked();
-                        setComplete(true);
-                        gitPush();
-                        setTimeout(() => window.open("https://dotori-2021.web.app/", "_blank"), 3000); //5초 딜레이
-                    }}
-                    style={{ backgroundColor: `${complete ? " #e5e5e5" : "#755e4c"}` }}
-                >
-                    도토리 내 집에 보관하기
-                </AddButton>
-            </ContentBox>
-            <PrevCommitsContainer>
-                {curLog.map((log, i) => {
-                    return (
-                        <PrevCommitBox clicked={clicked} onClick={toggleClicked} index={i}>
-                            <PrevCommit src={commit}></PrevCommit>
-                            <PrevCommitName>{log.name}</PrevCommitName>
-                        </PrevCommitBox>
-                    );
-                })}
-            </PrevCommitsContainer>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <ContentBox>
+        <PushBox>
+          <CommitBox clicked={clicked} onClick={toggleClicked}>
+            <CommitImg src={commit}></CommitImg>
+            <CommitName>{name}</CommitName>
+          </CommitBox>
+          <HomeImg src={home}></HomeImg>
+        </PushBox>
+        <AddButton
+          onClick={() => {
+            toggleClicked();
+            setComplete(true);
+            gitPush();
+            setTimeout(
+              () => window.open("https://dotori-2021.web.app/", "_blank"),
+              3000
+            ); //5초 딜레이
+          }}
+          style={{ backgroundColor: `${complete ? " #e5e5e5" : "#755e4c"}` }}
+        >
+          도토리 내 집에 보관하기
+        </AddButton>
+      </ContentBox>
+      <PrevCommitsContainer>
+        {curLog.map((log, i) => {
+          return (
+            <PrevCommitBox clicked={clicked} onClick={toggleClicked} index={i}>
+              <PrevCommit src={commit}></PrevCommit>
+              <PrevCommitName>{log.name}</PrevCommitName>
+            </PrevCommitBox>
+          );
+        })}
+      </PrevCommitsContainer>
+    </Wrapper>
+  );
 };
 
 export default GitPush;
